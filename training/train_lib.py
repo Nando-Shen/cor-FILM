8f359eeb02c26c6db4df5eb1335b7a8557cda2a3
shenjiaming <shenjiaming@rd.netease.com>
2022-07-28 17:56:17 +1000

    initial

--- training/train_lib.py
+++ training/train_lib.py
@@ -134,8 +134,8 @@ def train_loop(
     num_iterations: int,
     save_summaries_frequency: int = 500,
     save_checkpoint_frequency: int = 500,
-    checkpoint_max_to_keep: int = 10,
-    checkpoint_save_every_n_hours: float = 2.,
+    checkpoint_max_to_keep: int = 6,
+    checkpoint_save_every_n_hours: float = 3.,
     timing_frequency: int = 100,
     logging_frequency: int = 10):
   """A Tensorflow 2 eager mode training loop.
